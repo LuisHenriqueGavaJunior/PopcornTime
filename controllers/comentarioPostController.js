740a42c75b01fee64b1a8b6082215040a734ec8c
Luis Henrique <euluishenrique7@gmail.com>
2022-09-09 13:43:38 -0300

    Listar comentariopost e curtidapost feitos no Insomnia

--- controllers/comentarioPostController.js
+++ controllers/comentarioPostController.js
@@ -1,12 +1,15 @@
 //Luis 
 
-const { json } = require("express");
-const db = require("../database/connection");
+const { json } = require('express');
+const db = require('../database/connection');
 
 module.exports = {
     async listarComentarioPost(request, response){
         try{
-            return response.status(200).json({confirma: "ComentarioPost"}); 
+            const sql = 'Select id_comentario, id_post, id_usuario, comentario_post from comentariospost;';
+            const comentariopost = await db.query(sql);
+
+            return response.status(200).json({confirma: "Sucesso", nResults: comentariopost[0].length, message: comentariopost[0]}); 
         } catch (error){
             return response.status(500).json({confirma: 'Erro', message: error});
         }
--- controllers/curtidaPostController.js
+++ controllers/curtidaPostController.js
@@ -6,7 +6,10 @@ const db = require("../database/connection");
 module.exports = {
     async listarCurtidaPost(request, response){
         try{
-            return response.status(200).json({confirma: "CurtidaPost"}); 
+            const sql = 'Select id_post, id_usuario from curtidapost;';
+            const curtidapost = await db.query(sql);
+
+            return response.status(200).json({confirma: "Sucesso", nResults: curtidapost[0].length, message: curtidapost[0]}); 
         } catch (error){
             return response.status(500).json({confirma: 'Erro', message: error});
         }
